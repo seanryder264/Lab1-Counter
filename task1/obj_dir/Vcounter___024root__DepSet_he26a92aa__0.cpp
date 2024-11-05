f574dabbc4458d4c01767fc0d5b12855b3e3cc0f
twopigoens <seanryder264@outlook.com>
2024-11-05 13:58:33 +0000

    T1 fix counter 2

--- task1/obj_dir/Vcounter___024root__DepSet_he26a92aa__0.cpp
+++ task1/obj_dir/Vcounter___024root__DepSet_he26a92aa__0.cpp
@@ -11,7 +11,9 @@ VL_INLINE_OPT void Vcounter___024root___sequent__TOP__0(Vcounter___024root* vlSe
     Vcounter__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
     VL_DEBUG_IF(VL_DBG_MSGF("+    Vcounter___024root___sequent__TOP__0\n"); );
     // Body
-    vlSelf->count = ((IData)(vlSelf->rst) ? 0U : (IData)(vlSelf->en));
+    vlSelf->count = ((IData)(vlSelf->rst) ? 0U : (0xffU 
+                                                  & ((IData)(vlSelf->count) 
+                                                     + (IData)(vlSelf->en))));
 }
 
 void Vcounter___024root___eval(Vcounter___024root* vlSelf) {
